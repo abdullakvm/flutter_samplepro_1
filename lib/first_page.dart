adc67b48ed31df80cf32dd7e273b2fb5ed3012f7
abdullakvm <abdullakvm10@gmailcom>
2025-07-30 16:54:13 +0530

    container

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -7,7 +7,7 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.amberAccent,
+        backgroundColor: const Color.fromARGB(255, 20, 135, 193),
         leading: Icon(Icons.menu, color: Colors.white, size: 50),
         title: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
@@ -18,32 +18,45 @@ class Homepage extends StatelessWidget {
         actionsIconTheme: IconThemeData(color: Colors.white, size: 50),
       ),
       backgroundColor: Colors.redAccent,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SafeArea(
-              child: Center(
-                child: Text(
-                  "Abdulla",
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                    color: const Color.fromARGB(255, 11, 11, 202),
-                  ),
-                ),
+      body: Center(
+        child: Container(
+          
+          decoration: BoxDecoration(
+            border: Border.all(width: 10, color: Colors.white),
+
+            gradient: LinearGradient(
+              begin: Alignment.bottomLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                const Color.fromARGB(255, 223, 126, 120),
+                Colors.blueAccent,
+              ],
+            ),
+            // color: Colors.lightBlueAccent,
+            borderRadius: BorderRadius.circular(30),
+            image: DecorationImage(
+              // fit: BoxFit.cover, // when add image add as boxfit the gradient does not show
+              image: AssetImage("assets/image/mencard1.jpeg"),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.white.withValues(alpha: 0.7),
+                blurRadius: 30,
+                spreadRadius: 10,
+                offset: Offset(-10, 10),
               ),
-            ),
-            Image.network(
-              "https://images.pexels.com/photos/1131774/pexels-photo-1131774.jpeg",
-              height: 150,
-              width: 100,
-              fit: BoxFit.contain,
-            ),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(50),
-              child: Image.asset("assets/image/mencard1.jpeg"),
-            ),
-          ],
+            ],
+            // shape: BoxShape.circle // for getting circle shape for the container if this use border radius cant use
+          ),
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(10),
+
+          height: 200,
+          width: 200,
+          child: Text(
+            "Helooooooooooooooooooooooooooooooooooooooooooooo",
+            style: TextStyle(fontSize: 10),
+          ),
         ),
       ),
     );
