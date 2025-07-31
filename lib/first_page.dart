1c1bcb5947a174acad8e2bb0efa0063bc78cd6c9
abdullakvm <abdullakvm10@gmailcom>
2025-07-31 21:00:08 +0530

    avatar

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -18,46 +18,25 @@ class Homepage extends StatelessWidget {
         actionsIconTheme: IconThemeData(color: Colors.white, size: 50),
       ),
       backgroundColor: Colors.redAccent,
-      body: Center(
-        child: Container(
-          
-          decoration: BoxDecoration(
-            border: Border.all(width: 10, color: Colors.white),
-
-            gradient: LinearGradient(
-              begin: Alignment.bottomLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                const Color.fromARGB(255, 223, 126, 120),
-                Colors.blueAccent,
-              ],
-            ),
-            // color: Colors.lightBlueAccent,
-            borderRadius: BorderRadius.circular(30),
-            image: DecorationImage(
-              // fit: BoxFit.cover, // when add image add as boxfit the gradient does not show
-              image: AssetImage("assets/image/mencard1.jpeg"),
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.white.withValues(alpha: 0.7),
-                blurRadius: 30,
-                spreadRadius: 10,
-                offset: Offset(-10, 10),
-              ),
-            ],
-            // shape: BoxShape.circle // for getting circle shape for the container if this use border radius cant use
+      body: Column(
+        spacing: 50,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(),
+          CircleAvatar(
+            radius: 50,
+            backgroundColor: Colors.blueAccent,
+            backgroundImage: AssetImage("assets/image/mencard1.jpeg"),
+            child: Text("abd"),
           ),
-          alignment: Alignment.center,
-          padding: EdgeInsets.all(10),
 
-          height: 200,
-          width: 200,
-          child: Text(
-            "Helooooooooooooooooooooooooooooooooooooooooooooo",
-            style: TextStyle(fontSize: 10),
+          CircleAvatar(
+            radius: 50,
+            backgroundColor: Colors.blueAccent,
+            backgroundImage: AssetImage("assets/image/mencard1.jpeg"),
           ),
-        ),
+        ],
       ),
     );
   }
